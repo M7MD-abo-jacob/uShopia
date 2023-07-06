0b87d111ea3d96173c2026ef1427658634f21140
M7MD-abo-jacob <105122190+M7MD-abo-jacob@users.noreply.github.com>
2023-07-06 15:56:50 +0300

    changed font

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ const MyDocument = ({ __NEXT_DATA__: { locale } }) => {
   return (
     <Html dir="ltr" lang={locale}>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.svg" />
         <meta
           name="keywords"
